7e79f9346847b4cd17570d58d0369ac7705c7a06
siad007 <siad.ardroumli@gmail.com>
2015-05-14 18:48:03 +0200

    Added first implementations

---
+++ src/Assume.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Assumptions
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * @copyright 2015 MehrAlsNix (http://www.mehralsnix.de)
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT
+ * @link      http://github.com/MehrAlsNix/kindergarten
+ */
+
+namespace MehrAlsNix\Assumptions;
+
+use Hamcrest\Matcher;
+
+/**
+ * Class Assume
+ * @package MehrAlsNix\Assumptions
+ */
+class Assume
+{
+    /**
+     * @param boolean $bool
+     * @param string $message optional
+     *
+     * @throws AssumptionViolatedException
+     */
+    public static function assumeTrue($bool, $message = '')
+    {
+        self::assumeThat($bool, is(true), $message);
+    }
+
+    /**
+     * @param boolean $bool
+     * @param string $message
+     *
+     * @throws AssumptionViolatedException
+     */
+    public static function assumeFalse($bool, $message = '')
+    {
+        self::assumeThat($bool, is(false), $message);
+    }
+
+    /**
+     * @param mixed $actual
+     * @param Matcher $matcher
+     * @param string $message optional
+     *
+     * @throws AssumptionViolatedException
+     */
+    public static function assumeThat($actual, Matcher $matcher, $message = '')
+    {
+        if (!$matcher->matches($actual)) {
+            throw new AssumptionViolatedException($actual, $matcher, $message);
+        }
+    }
+
+    /**
+     * @param string $message
+     * @param \Exception $e
+     *
+     * @throws AssumptionViolatedException
+     */
+    public static function assumeNoException($message, \Exception $e)
+    {
+        self::assumeThat($message, $e, nullValue());
+    }
+}
---
+++ src/AssumptionViolatedException.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Assumptions
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * @copyright 2015 MehrAlsNix (http://www.mehralsnix.de)
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT
+ * @link      http://github.com/MehrAlsNix/kindergarten
+ */
+
+namespace MehrAlsNix\Assumptions;
+
+use Hamcrest\Matcher;
+
+/**
+ * Class AssumptionViolatedException
+ * @package MehrAlsNix\Assumptions
+ */
+class AssumptionViolatedException extends \RuntimeException
+{
+    /**
+     * @var string
+     */
+    private $assumption;
+
+    /**
+     * @var
+     */
+    private $valueMatcher;
+
+    /**
+     * @var
+     */
+    private $value;
+
+    /**
+     * @var Matcher
+     */
+    private $matcher;
+
+    /**
+     * {@inheritdoc}
+     *
+     * @param string $assumption
+     * @param Matcher $matcher
+     * @param string $message
+     */
+    public function __construct($assumption, Matcher $matcher, $message = '')
+    {
+        $this->assumption = $assumption;
+        $this->matcher    = $matcher;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getMessage()
+    {
+
+    }
+}
